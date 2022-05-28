7d6537e941f617d07608198237f44d0bd8e75b9f
NDedov <ndedov@gmail.com>
2022-05-28 22:06:17 +0700

    lesson2

--- src/main/java/ru/dedov/MyArray.java
+++ src/main/java/ru/dedov/MyArray.java
@@ -1,5 +1,10 @@
 package ru.dedov;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class MyArray {
     private int[] arr;
     private int capacity;
@@ -27,6 +32,14 @@ public class MyArray {
         return arr[idx];
     }
 
+    public void random(int amount, int bound){
+        Random rnd = new Random();
+        capacity = 0;
+        for (int i = 0; i < amount; i++) {
+            this.append(rnd.nextInt(bound));
+        }
+    }
+
     public void set(int value, int idx) {
         arr[idx] = value;
     }
@@ -42,6 +55,34 @@ public class MyArray {
         return false;
     }
 
+    void insert(int idx, int value){
+        if (this.capacity == this.arr.length) {
+            int[] old = this.arr;
+            this.arr = new int[old.length * 2];
+            System.arraycopy(old, 0, arr, 0, old.length);
+        }
+
+        System.arraycopy(arr,idx, arr,idx+1,capacity-idx);
+        capacity++;
+        arr[idx] = value;
+    }
+
+    boolean deleteAll(int value) {
+        boolean fResult = false;
+        for (int i = 0; i < this.capacity; i++) {
+            if (this.arr[i] == value) {
+                System.arraycopy(this.arr, i + 1, this.arr, i, this.capacity - i - 1);
+                --capacity;
+                --i;
+                fResult = true;
+            }
+        }
+        return fResult;
+    }
+    boolean deleteAll (){
+        capacity = 0;
+        return true;
+    }
     void append(int value) {
         if (this.capacity == this.arr.length) {
             int[] old = this.arr;
@@ -112,5 +153,78 @@ public class MyArray {
             }
             this.arr[move] = temp;
         }
+    }
+
+    public long sortBubbleCount() {
+        long count = 0;
+        for (int iter = 0; iter < capacity; iter++)
+            for (int idx = 0; idx < capacity - 1; idx++) {
+                if (this.arr[idx] > this.arr[idx + 1])
+                    swap(idx, idx + 1);
+                count++;
+            }
+
+        return count;
+    }
+
+    public long sortBubbleCountMod() {
+        long count = 0;
+        for (int iter = 0; iter < capacity; iter++)
+            for (int idx = 0; idx < capacity - iter - 1; idx++) {
+                if (this.arr[idx] > this.arr[idx + 1])
+                    swap(idx, idx + 1);
+                count++;
+            }
+
+        return count;
+    }
+
+    public long sortSelectCount() {
+        long count = 0;
+        for (int idx = 0; idx < capacity; idx++) {
+            int curr = idx;
+            for (int srch = idx + 1; srch < capacity; srch++){
+                if (this.arr[srch] < this.arr[curr])
+                    curr = srch;
+                if (curr != idx)
+                    swap(idx, curr);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public long sortInsertCount() {
+        long count = 0;
+        for (int curr = 1; curr < capacity; curr++) {
+            int temp = this.arr[curr];
+            int move = curr;
+            while (move > 0 && this.arr[move - 1] >= temp) {
+                this.arr[move] = this.arr[move - 1];
+                count++;
+                move--;
+            }
+            this.arr[move] = temp;
+        }
+        return count;
+    }
+
+    public void sortCounting(){
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < capacity; i++)
+            if (hm.get(arr[i]) != null)
+                hm.put(arr[i], hm.get(arr[i]) + 1);
+            else
+                hm.put(arr[i], 1);
+
+        capacity = 0;
+        for (Map.Entry<Integer, Integer> item:hm.entrySet()) {
+            int[] tmpArray = new int[item.getValue()];
+            Arrays.fill(tmpArray, item.getKey());
+            System.arraycopy(tmpArray,0,arr,capacity,item.getValue());
+            capacity += item.getValue();
+        }
+
     }
 }
--- src/main/java/ru/dedov/hw2.java
+++ src/main/java/ru/dedov/hw2.java
@@ -1,4 +1,69 @@
 package ru.dedov;
 
 public class hw2 {
+    public static void main(String[] args) {
+
+        System.out.println("Удаление всех 1");
+        MyArray arr = new MyArray(new int[]{1,1,2,1,34,1,1,1,1,1,45,23,1,1});
+        arr.display();
+        arr.deleteAll(1);
+        arr.display();
+
+        System.out.println("Очистка массива");
+        MyArray arr01 = new MyArray(new int[]{1,1,2,1,34,1,1,1,1,1,45,23,1,1});
+        arr01.display();
+        arr01.deleteAll();
+        arr01.display();
+        arr01.append(125);
+        arr01.display();
+
+        System.out.println("Вставка в массив");
+        MyArray arr02 = new MyArray(new int[]{1,2,3,4,5,6,7,8,9,10});
+        arr02.display();
+        arr02.insert(2,100);
+        arr02.display();
+        arr02.insert(0,100);
+        arr02.display();
+        arr02.insert(12,100);
+        arr02.display();
+
+        MyArray arr03 = new MyArray(1);
+        int arrayLen = 10_000;
+        int max = 1000;
+        long actions;
+        long currentTime;
+        double time;
+
+        arr03.random(arrayLen, max);
+        currentTime = System.nanoTime();
+        actions = arr03.sortBubbleCount();
+        time = (System.nanoTime() - currentTime) / 1_000_000_000d;
+        System.out.println("Пузырьком выполнено действий: " + actions + " за " + time + "  секунд");
+
+        arr03.random(arrayLen, max);
+        currentTime = System.nanoTime();
+        actions = arr03.sortSelectCount();
+        time = (System.nanoTime() - currentTime) / 1_000_000_000d;
+        System.out.println("Выбором выполнено действий: " + actions + " за " + time + "  секунд");
+
+        arr03.random(arrayLen, max);
+        currentTime = System.nanoTime();
+        actions = arr03.sortInsertCount();
+        time = (System.nanoTime() - currentTime) / 1_000_000_000d;
+        System.out.println("Вставкой выполнено действий: " + actions + " за " + time + "  секунд");
+
+        arr03.random(arrayLen, max);
+        currentTime = System.nanoTime();
+        actions = arr03.sortBubbleCountMod();
+        time = (System.nanoTime() - currentTime) / 1_000_000_000d;
+        System.out.println("Модифицированным пузырьком выполнено действий: " + actions + " за " + time + "  секунд");
+
+        arr03.random(arrayLen, max);
+        currentTime = System.nanoTime();
+        arr03.sortCounting();
+        time = (System.nanoTime() - currentTime) / 1_000_000_000d;
+        System.out.println("Сортировка подсчетом выполнена за " + time + "  секунд");
+
+
+    }
 }
